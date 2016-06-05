acddb0d3bbfb1aeb8b440f37cd45b400f1af9196
ivy <tangolivesky@163.com>
2016-06-06 07:09:27 +0800

    优化audioplayer

--- audioplayer.js
+++ audioplayer.js
@@ -14,9 +14,20 @@ var AudioPlayer = (function () {
         var timeProgressBar = document.createElement("span");
         var playhead = document.createElement("div");
         var currentTime = document.createElement("div");
-        var timelineWidth;
+        var audioLine = document.createElement("div");
+        var audioLineBar = document.createElement("span");
+        var audioLineHead = document.createElement("div");
+
+        var timelineWidth =230;
+        var audiolinewidth = 60;
+        var onplayhead = false;
 
         this.myAudioPlayer = myAudioPlayer;
+        this.playhead = playhead;
+        this.timeProgressBar = timeProgressBar;
+        this.timelineWidth = timelineWidth;
+        this.playButton = playButton;
+        this.currentTime = currentTime;
 
 
         myAudioPlayer.className = "audioplayer";
@@ -30,26 +41,97 @@ var AudioPlayer = (function () {
         }
         audioPlayer.setAttribute('controls', 'controls');
         audioPlayer.style.display = "none";
+        audioLine.className= "audio-line";
+        audioLineHead.className = "audio-line-head";
+        audioLineBar.className = "audio-line-bar";
 
         myAudioPlayer.appendChild(playButton);
         timeLine.appendChild(timeProgressBar);
         timeLine.appendChild(playhead);
         myAudioPlayer.appendChild(timeLine);
+        myAudioPlayer.appendChild(currentTime);
         myAudioPlayer.appendChild(audioPlayer);
+        audioLine.appendChild(audioLineBar);
+        audioLine.appendChild(audioLineHead);
+        myAudioPlayer.appendChild(audioLine);
 
-        timelineWidth = timeLine.offsetWidth - playhead.offsetWidth;
+        //timelineWidth = 230;
+        //timeLine.offsetWidth - playhead.offsetWidth;
 
 
         playButton.addEventListener('click', play, false);
         audioPlayer.addEventListener("timeupdate", timeUpdate, false);
-        // Gets audio file duration
         audioPlayer.addEventListener("canplaythrough", function () {
             duration = audioPlayer.duration;
         }, false);
+        timeLine.addEventListener("click",function (event) {
+            moveplayhead(event);
+            audioPlayer.currentTime = duration * clickPercent(event);
+        }, false);
+        playhead.addEventListener('mousedown', mouseDown, false);
+        window.addEventListener('mouseup', mouseUp, false);
+        audioLine.addEventListener("click",function(event){
+            moveaudiohead(event);
+            audioPlayer.volume = volumeClickPercent(event);
+        },false);
 
+        function mouseDown() {
+            onplayhead = true;
+            window.addEventListener('mousemove', moveplayhead, true);
+            audioPlayer.removeEventListener('timeupdate', timeUpdate, false);
+        }
 
+        function mouseUp(e) {
+            if (onplayhead == true) {
+                moveplayhead(e);
+                window.removeEventListener('mousemove', moveplayhead, true);
+                // change current time
+                audioPlayer.currentTime = duration * clickPercent(e);
+                audioPlayer.addEventListener('timeupdate', timeUpdate, false);
+            }
+            onplayhead = false;
+        }
 
-        //Play and Pause
+        function clickPercent(e) {
+            return (e.pageX - timeLine.offsetLeft) / timelineWidth;
+        }
+
+        function volumeClickPercent(e){
+            return (e.pageX - audioLine.offsetLeft)/audiolinewidth;
+        }
+
+        function moveplayhead(e) {
+            var newMargLeft = e.pageX - timeLine.offsetLeft;
+            if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+                playhead.style.marginLeft = newMargLeft + "px";
+                timeProgressBar.style.width = newMargLeft*100/timelineWidth + "%";
+            }
+            if (newMargLeft < 0) {
+                playhead.style.marginLeft = "0px";
+                timeProgressBar.style.width = "0%";
+            }
+            if (newMargLeft > timelineWidth) {
+                playhead.style.marginLeft = timelineWidth + "px";
+                timeProgressBar.style.width = "100%";
+            }
+        }
+
+        function moveaudiohead(e) {
+            var newMargLeft = e.pageX - audioLine.offsetLeft;
+            if (newMargLeft >= 0 && newMargLeft <= audiolinewidth) {
+                audioLineHead.style.marginLeft = newMargLeft + "px";
+                audioLineBar.style.width = newMargLeft*100/audiolinewidth + "%";
+            }
+            if (newMargLeft < 0) {
+                audioLineHead.style.marginLeft = "0px";
+                audioLineBar.style.width = "0%";
+            }
+            if (newMargLeft > timelineWidth) {
+                audioLineHead.style.marginLeft = audiolinewidth + "px";
+                audioLineBar.style.width = "100%";
+            }
+        }
+
         function play() {
             // start music
             if (audioPlayer.paused) {
@@ -67,8 +149,8 @@ var AudioPlayer = (function () {
 
         function timeUpdate(){
             var playPercent = audioPlayer.currentTime / duration;
-            var playPercentWidth = timelineWidth*(playPercent);
-            //currentTime[0].innerHTML = audioPlayer.currentTime;
+            var playPercentWidth = timelineWidth*playPercent;
+            currentTime.innerHTML = (audioPlayer.currentTime/60).toFixed(2);
             playhead.style.marginLeft = playPercentWidth + "px";
             timeProgressBar.style.width = playPercent*100 + "%";
             if (playPercentWidth.currentTime == duration) {
@@ -83,82 +165,34 @@ var AudioPlayer = (function () {
         return this.myAudioPlayer;
     };
 
+    AudioPlayer.prototype.record = function(recordTime){
+
+        var ct = 0;
+        var playhead = this.playhead;
+        var timeLineWidth = this.timelineWidth;
+        var timeProgressBar = this.timeProgressBar;
+        var playButton = this.playButton;
+        var currentTime = this.currentTime;
+        playButton.setAttribute("disabled","true");
+
+        var recoding = setInterval(
+        function showProgress(){
+            ct = ct+0.5;
+            currentTime.innerHTML = ct;
+            var playPercent = ct/recordTime;
+            var playPercentWidth = timeLineWidth * playPercent;
+            playhead.style.marginLeft = playPercentWidth + "px";
+            timeProgressBar.style.width = playPercent*100 + "%";
+        },500);
+
+        setTimeout(function stopProgress(){
+            clearInterval(recoding);
+            playButton.removeAttribute("disabled");
+
+        },recordTime*1000);
+
+
+    };
+
     return AudioPlayer;
 })();
-
-
-
-// function audioPlayer(cfg){
-//
-//     var config = cfg || {};
-//     var myAudioPlayer = document.createElement("div");
-//     var audioPlayer = document.createElement("audio");
-//     var playButton = document.createElement("button");
-//     var timeLine = document.createElement("div");
-//     var timeProgressBar = document.createElement("span");
-//     var playhead = document.createElement("div");
-//     var currentTime = document.createElement("div");
-//
-//     this.initMyAudioPlayer = function () {
-//
-//         myAudioPlayer.className = "audioplayer";
-//
-//         playButton.className = "playbutton play";
-//
-//         timeLine.className = "timeline";
-//
-//         timeProgressBar.className = "time-progress-bar";
-//
-//         playhead.className = "playhead";
-//
-//         currentTime.className = "current-time";
-//
-//         if(config.hasOwnProperty("audio")) {
-//             if(config.audio.hasOwnProperty("src"))audioPlayer.setAttribute('src',config.audio.src);
-//         }
-//         audioPlayer.setAttribute('controls', 'controls');
-//         audioPlayer.style.display = "none";
-//
-//
-//         myAudioPlayer.appendChild(playButton);
-//         timeLine.appendChild(timeProgressBar);
-//         timeLine.appendChild(playhead);
-//         myAudioPlayer.appendChild(timeLine);
-//         myAudioPlayer.appendChild(audioPlayer);
-//
-//         //audioPlayer.load();
-//         //audioPlayer.play();
-//
-//         playButton.addEventListener('click',play,false);
-//     };
-//
-//     //Play and Pause
-//     var play =function() {
-//         // start music
-//         if (audioPlayer.paused) {
-//             audioPlayer.play();
-//             // remove play, add pause
-//             playButton.className = "";
-//             playButton.className = "playbutton pause";
-//         } else { // pause music
-//             audioPlayer.pause();
-//             // remove pause, add play
-//             playButton.className = "";
-//             playButton.className = "playbutton play";
-//         }
-//     }
-//
-//
-//     this.getAudioPlayer = function () {
-//         return myAudioPlayer;
-//     }
-//
-//
-//
-//     //initMyAudioPlayer();
-//
-// }
-
-//     window.audioPlayer = audioPlayer;
-//
-// })(window);
